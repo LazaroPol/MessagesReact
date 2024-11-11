fbeea44e4cfaf7e9a3df521e89e5650c85ba24af
LazaroPol <pol.lazaro.rosas@gmail.com>
2024-11-11 19:42:23 +0100

    create is not working

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,30 +3,50 @@ import { getMessagesById, createMessage } from "../services/api";
 import { useUserContext } from "../provider/UserProvider";
 
 const Message = () => {
-  const [message, setMessage] = useState([]);
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState({
+    text: "",
+    destinatarios: "",
+    remitente: "",
+  });
   const [user] = useUserContext();
 
-  console.log("Usuariooooooooooooo", user);
   const handleclick = () => {
-    if (message.length === 0) {
-      getMessagesById(user.id).then(setMessage);
+    if (messageList.length === 0) {
+      getMessagesById(user.id).then(setMessageList);
     } else {
-      setMessage([]);
+      setMessageList([]);
     }
   };
 
-  const createMsg = () => {};
+  const createMsg = () => {
+    createMessage(message);
+  };
 
   return (
     <div>
       <button onClick={handleclick}>Ver mensajes</button> <br />
       <br />
-      <button>Crear mensaje</button>
-      {message.map((msg) => (
+      <button onClick={createMsg}>Crear mensaje</button>
+      {messageList.map((msg) => (
         <div key={msg.id}>
           <p>{msg.text}</p>
         </div>
       ))}
+      <input
+        type="text"
+        value={message.text}
+        onChange={(event) =>
+          setMessage({ ...message, text: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        value={message.destinatarios}
+        onChange={(event) =>
+          setMessage({ ...message, destinatarios: event.target.value })
+        }
+      />
     </div>
   );
 };
